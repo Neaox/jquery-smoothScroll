a5b84547423cf5de565c9bb1c39209e953886a27
Nico Niebergall <nico.niebergall@artif.com>
2013-12-13 18:00:55 +0100

    Initial commit

---
+++ jquery.smoothscroll.js
@@ -0,0 +1,39 @@
+/*!
+ * smoothScroll - jQuery Plugin
+ * version: 1.0 (Fri, 13 Dec 2013)
+ *
+ * License: GNU GPL v2
+ * Copyright 2013 Nico Niebergall - nico@nightowl.me
+ *
+ */
+
+(function ($) {
+	jQuery.fn.smoothScroll = function(options) {
+		var settings = jQuery.extend({
+			duration: 750,
+			easing: 'linear',
+			offset: 0,
+			before: function() {},
+			after: function() {},
+			toTop: null,
+		}, options);
+
+		return this.each(function() {
+			$(this).click(function(e) {
+				var target;
+				e.preventDefault();
+				
+				target = (settings.toTop) ? target = 'html' : target = this.hash;
+				
+				// callback: before animation
+				if(settings.before) {
+					settings.before();
+				}
+				
+				$('html').animate({
+					scrollTop: $(target).offset().top + parseInt(settings.offset)
+				}, settings.duration, settings.easing, settings.after());
+			});
+		});
+	};
+}(jQuery));
